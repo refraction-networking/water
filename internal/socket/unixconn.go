e8cc8e6bdeced3115617d990f33b551714c1db66
Gaukas Wang <i@gauk.as>
2023-10-10 13:27:49 -0600

    fix: unsafe defer and add examples (#4)
    * fix: unsafe defer
    
    * new: add examples

--- internal/socket/unixconn.go
+++ internal/socket/unixconn.go
@@ -109,7 +109,6 @@ func UnixConnPair(path string) (c1, c2 net.Conn, err error) {
 	if err != nil {
 		return nil, nil, fmt.Errorf("net.Listen returned error: %w", err)
 	}
-	defer ul.Close()
 
 	var wg *sync.WaitGroup = new(sync.WaitGroup)
 	var goroutineErr error
@@ -134,37 +133,5 @@ func UnixConnPair(path string) (c1, c2 net.Conn, err error) {
 		return nil, nil, fmt.Errorf("c1 or c2 is nil")
 	}
 
-	return c1, c2, nil
-}
-
-func TCPConnPair(address string) (c1, c2 net.Conn, err error) {
-	l, err := net.Listen("tcp", address)
-	if err != nil {
-		return nil, nil, fmt.Errorf("net.Listen returned error: %w", err)
-	}
-	defer l.Close()
-
-	var wg *sync.WaitGroup = new(sync.WaitGroup)
-	var goroutineErr error
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		c2, goroutineErr = l.Accept()
-	}()
-
-	c1, err = net.Dial("tcp", l.Addr().String())
-	if err != nil {
-		return nil, nil, fmt.Errorf("net.Dial returned error: %w", err)
-	}
-	wg.Wait()
-
-	if goroutineErr != nil {
-		return nil, nil, fmt.Errorf("l.Accept returned error: %w", goroutineErr)
-	}
-
-	if c1 == nil || c2 == nil {
-		return nil, nil, fmt.Errorf("c1 or c2 is nil")
-	}
-
-	return c1, c2, nil
+	return c1, c2, ul.Close()
 }
