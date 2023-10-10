baa9b44e2d48a345e72d41e51409bad7a0c259b0
Gaukas Wang <i@gauk.as>
2023-10-10 16:02:46 -0600

    fix: unixconnpair non-fatal error handling (#6)

--- internal/log/slog_old.go
+++ internal/log/slog_old.go
@@ -8,23 +8,29 @@ import (
 	"golang.org/x/exp/slog"
 )
 
+var slogger *slog.Logger = slog.Default()
+
+func ReplaceLogger(logger *slog.Logger) {
+	slogger = logger
+}
+
 func Debugf(format string, args ...any) {
-	slog.Default().Debug(fmt.Sprintf(format, args...))
+	slogger.Debug(fmt.Sprintf(format, args...))
 }
 
 func Infof(format string, args ...any) {
-	slog.Default().Info(fmt.Sprintf(format, args...))
+	slogger.Info(fmt.Sprintf(format, args...))
 }
 
-func Warnf(format string, args ...any) {
-	slog.Default().Warn(fmt.Sprintf(format, args...))
+func Warningf(format string, args ...any) {
+	slogger.Warn(fmt.Sprintf(format, args...))
 }
 
 func Errorf(format string, args ...any) {
-	slog.Default().Error(fmt.Sprintf(format, args...))
+	slogger.Error(fmt.Sprintf(format, args...))
 }
 
 func Fatalf(format string, args ...any) {
-	slog.Default().Error(fmt.Sprintf(format, args...))
+	slogger.Error(fmt.Sprintf(format, args...))
 	panic("fatal error occurred")
 }
