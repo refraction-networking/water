8da998ab92f1ce1082e95da57c9d279d401237cb
Gaukas Wang <i@gaukas.wang>
2023-11-28 15:30:21 -0700

    update: canonical test cases

--- transport/v0/dialer_test.go
+++ transport/v0/dialer_test.go
@@ -14,8 +14,10 @@ import (
 )
 
 func TestDialer(t *testing.T) {
+	loadPlain()
 	loadReverse()
 	t.Run("plain must work", testDialerPlain)
+	t.Run("reverse must work", testDialerReverse)
 	t.Run("bad addr must fail", testDialerBadAddr)
 	t.Run("partial WATM must fail", testDialerPartialWATM)
 }
@@ -23,7 +25,7 @@ func TestDialer(t *testing.T) {
 func testDialerBadAddr(t *testing.T) {
 	// Dial
 	config := &water.Config{
-		TMBin: reverse,
+		TMBin: plain,
 	}
 
 	dialer, err := water.NewDialer(config)
@@ -42,6 +44,151 @@ func testDialerBadAddr(t *testing.T) {
 }
 
 func testDialerPlain(t *testing.T) { // skipcq: GO-R1005
+	tcpLis, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// goroutine to accept incoming connections
+	var peerConn net.Conn
+	var goroutineErr error
+	var wg *sync.WaitGroup = new(sync.WaitGroup)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		peerConn, goroutineErr = tcpLis.Accept()
+	}()
+
+	// Dial using water
+	config := &water.Config{
+		TMBin: plain,
+	}
+	dialer, err := water.NewDialer(config)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	conn, err := dialer.Dial("tcp", tcpLis.Addr().String())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close() // skipcq: GO-S2307
+
+	// type assertion: conn must be *v0.Conn
+	if _, ok := conn.(*v0.Conn); !ok {
+		t.Fatalf("returned conn is not *v0.Conn")
+	}
+
+	wg.Wait()
+	if goroutineErr != nil {
+		t.Fatal(goroutineErr)
+	}
+	defer peerConn.Close() // skipcq: GO-S2307
+
+	// trigger garbage collection for several times to simulate any
+	// possible GC in the real world use case
+	runtime.GC()
+	time.Sleep(100 * time.Microsecond)
+	runtime.GC()
+	time.Sleep(100 * time.Microsecond)
+	runtime.GC()
+	time.Sleep(100 * time.Microsecond)
+
+	var waterSendBuf []byte = make([]byte, 1024)
+	var peerSendBuf []byte = make([]byte, 1024)
+	var waterRecvBuf []byte = make([]byte, 1024)
+	var peerRecvBuf []byte = make([]byte, 1024)
+	// send 10 messages in each direction
+	for i := 0; i < 10; i++ {
+		_, err = rand.Read(waterSendBuf)
+		if err != nil {
+			t.Fatalf("rand.Read error: %s", err)
+		}
+
+		_, err = rand.Read(peerSendBuf)
+		if err != nil {
+			t.Fatalf("rand.Read error: %s", err)
+		}
+
+		// dialer -> listener
+		_, err = conn.Write(waterSendBuf)
+		if err != nil {
+			t.Fatalf("conn.Write error: %s", err)
+		}
+
+		n, err := peerConn.Read(peerRecvBuf)
+		if err != nil {
+			t.Fatalf("peerConn.Read error: %s", err)
+		}
+
+		if n != len(waterSendBuf) {
+			t.Fatalf("peerConn.Read error: read %d bytes, want %d bytes", n, len(waterSendBuf))
+		}
+
+		if !bytes.Equal(peerRecvBuf[:n], waterSendBuf) {
+			t.Fatalf("peerRecvBuf != waterSendBuf")
+		}
+
+		// listener -> dialer
+		_, err = peerConn.Write(peerSendBuf)
+		if err != nil {
+			t.Fatalf("peerConn.Write error: %s", err)
+		}
+
+		n, err = conn.Read(waterRecvBuf)
+		if err != nil {
+			t.Fatalf("conn.Read error: %s", err)
+		}
+
+		if n != len(peerSendBuf) {
+			t.Fatalf("conn.Read error: read %d bytes, want %d bytes", n, len(peerSendBuf))
+		}
+
+		if !bytes.Equal(waterRecvBuf[:n], peerSendBuf) {
+			t.Fatalf("waterRecvBuf != peerSendBuf")
+		}
+
+		// trigger garbage collection
+		runtime.GC()
+		time.Sleep(100 * time.Microsecond)
+	}
+
+	// reading with a deadline
+	err = conn.SetDeadline(time.Now().Add(100 * time.Millisecond))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = conn.Read(waterRecvBuf)
+	if err == nil {
+		t.Fatalf("conn.Read must timeout")
+	}
+
+	if err := conn.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	// after closing the conn, read/write MUST fail
+	_, err = conn.Write(waterSendBuf)
+	if err == nil {
+		t.Fatalf("conn.Write must fail after closing the conn")
+	}
+
+	_, err = conn.Read(waterRecvBuf)
+	if err == nil {
+		t.Fatalf("conn.Read must fail after closing the conn")
+	}
+
+	if err := tcpLis.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	// trigger garbage collection
+	runtime.GC()
+	time.Sleep(100 * time.Microsecond)
+}
+
+func testDialerReverse(t *testing.T) { // skipcq: GO-R1005
 	tcpLis, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
 		t.Fatal(err)
@@ -201,7 +348,7 @@ func testDialerPartialWATM(t *testing.T) {
 }
 
 // BenchmarkDialerOutbound currently measures only the outbound throughput
-// of the dialer. Inbound throughput is not measured at the moment.
+// of the dialer. Inbound throughput is measured for the listener instead.
 //
 // Separate benchmark for the latency measurement will be needed.
 func BenchmarkDialerOutbound(b *testing.B) {
@@ -259,7 +406,15 @@ func BenchmarkDialerOutbound(b *testing.B) {
 	}
 }
 
-func BenchmarkReverseDialerOutbound(b *testing.B) {
+// BenchmarkDialerOutboundReverse currently measures only the outbound throughput
+// of the dialer. Inbound throughput is measured for the listener instead.
+//
+// Different from BenchmarkDialerOutbound, this benchmark uses the reverse
+// WebAssembly Transport Module, which reverse the bytes of each message before
+// sending it to the peer.
+//
+// Separate benchmark for the latency measurement will be needed.
+func BenchmarkDialerOutboundReverse(b *testing.B) {
 	loadReverse()
 	// create random TCP listener listening on localhost
 	tcpLis, err := net.ListenTCP("tcp", nil)
--- transport/v0/relay_test.go
+++ transport/v0/relay_test.go
@@ -13,12 +13,159 @@ import (
 )
 
 func TestRelay(t *testing.T) {
+	loadPlain()
 	loadReverse()
 	t.Run("plain must work", testRelayPlain)
+	t.Run("reverse must work", testRelayReverse)
 }
 
 func testRelayPlain(t *testing.T) { // skipcq: GO-R1005
-	// t.Skip()
+	// test destination: a local TCP server
+	tcpLis, err := net.ListenTCP("tcp", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// goroutine to accept incoming connections
+	var serverConn net.Conn
+	var serverAcceptErr error
+	var serverAcceptWg *sync.WaitGroup = new(sync.WaitGroup)
+	serverAcceptWg.Add(1)
+	go func() {
+		serverConn, serverAcceptErr = tcpLis.Accept()
+		serverAcceptWg.Done()
+	}()
+
+	// setup relay
+	config := &water.Config{
+		TMBin: plain,
+	}
+	relay, err := water.NewRelay(config)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// in a goroutine, start relay
+	var relayErr error
+	var relayWg *sync.WaitGroup = new(sync.WaitGroup)
+	relayWg.Add(1)
+	go func() {
+		relayErr = relay.ListenAndRelayTo("tcp", "localhost:0", "tcp", tcpLis.Addr().String())
+		relayWg.Done()
+	}()
+	time.Sleep(100 * time.Millisecond) // 100ms to spin up relay
+
+	// test source: a local TCP client
+	clientConn, err := net.Dial("tcp", relay.Addr().String())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer clientConn.Close() // skipcq: GO-S2307
+
+	// wait for server to accept connection
+	serverAcceptWg.Wait()
+	if serverAcceptErr != nil {
+		t.Fatal(serverAcceptErr)
+	}
+	defer serverConn.Close() // skipcq: GO-S2307
+
+	// trigger garbage collection for several times to simulate any
+	// possible GC in the real world use case
+	runtime.GC()
+	time.Sleep(100 * time.Microsecond)
+	runtime.GC()
+	time.Sleep(100 * time.Microsecond)
+	runtime.GC()
+	time.Sleep(100 * time.Microsecond)
+
+	var clientSendBuf []byte = make([]byte, 1024)
+	var serverSendBuf []byte = make([]byte, 1024)
+	var clientRecvBuf []byte = make([]byte, 1024)
+	var serverRecvBuf []byte = make([]byte, 1024)
+	// send 10 messages in each direction
+	for i := 0; i < 10; i++ {
+		_, err = rand.Read(clientSendBuf)
+		if err != nil {
+			t.Fatalf("rand.Read error: %s", err)
+		}
+
+		_, err = rand.Read(serverSendBuf)
+		if err != nil {
+			t.Fatalf("rand.Read error: %s", err)
+		}
+
+		// client -> server
+		_, err = clientConn.Write(clientSendBuf)
+		if err != nil {
+			t.Fatalf("conn.Write error: %s", err)
+		}
+
+		n, err := serverConn.Read(serverRecvBuf)
+		if err != nil {
+			t.Fatalf("serverConn.Read error: %s", err)
+		}
+
+		if n != len(clientSendBuf) {
+			t.Fatalf("serverConn.Read error: read %d bytes, want %d bytes", n, len(clientSendBuf))
+		}
+
+		if !bytes.Equal(serverRecvBuf[:n], clientSendBuf) {
+			t.Fatalf("serverRecvBuf != clientSendBuf")
+		}
+
+		// server -> client
+		_, err = serverConn.Write(serverSendBuf)
+		if err != nil {
+			t.Fatalf("serverConn.Write error: %s", err)
+		}
+
+		n, err = clientConn.Read(clientRecvBuf)
+		if err != nil {
+			t.Fatalf("clientConn.Read error: %s", err)
+		}
+
+		if n != len(serverSendBuf) {
+			t.Fatalf("clientConn.Read error: read %d bytes, want %d bytes", n, len(serverSendBuf))
+		}
+
+		if !bytes.Equal(clientRecvBuf[:n], serverSendBuf) {
+			t.Fatalf("clientRecvBuf != serverSendBuf")
+		}
+
+		// trigger garbage collection
+		runtime.GC()
+		time.Sleep(100 * time.Microsecond)
+	}
+
+	// stop relay
+	err = relay.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// wait for relay to stop
+	relayWg.Wait()
+	if relayErr != nil {
+		t.Fatal(relayErr)
+	}
+
+	// at this time, connection must still be alive
+	_, err = clientConn.Write([]byte("hello"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	n, err := serverConn.Read(serverRecvBuf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(serverRecvBuf[:n]) != "hello" {
+		t.Fatalf("serverRecvBuf != \"hello\"")
+	}
+}
+
+func testRelayReverse(t *testing.T) { // skipcq: GO-R1005
 	// test destination: a local TCP server
 	tcpLis, err := net.ListenTCP("tcp", nil)
 	if err != nil {
